b3d912c7d8f08f0f500792772c8c797bd8a0a585
Tembo <wickedtoewhisper@gmail.com>
2026-05-18 08:17:13 +0200

    Fix LinkedIn URL

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 const socials = [
   {
     label: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/saidi-tembo',
+    href: 'https://www.linkedin.com/in/saidi-tembo-a95554141/',
     icon: (
       <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
         <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
